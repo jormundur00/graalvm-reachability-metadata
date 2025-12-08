db2ef4aba12a98ed7db9976e2a7232368e22c1d7
jvukicev <jovan.vukicevic@oracle.com>
2025-12-08 09:57:48 +0100

    test changing delete detection

--- tests/src/io.grpc/grpc-core/1.69.0/src/test/java/grpc/GrpcTests.java
+++ tests/src/io.grpc/grpc-core/1.69.0/src/test/java/grpc/GrpcTests.java
@@ -44,6 +44,5 @@ class SimpleService extends SimpleGrpc.SimpleImplBase {
                 .build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
-        // asdf
     }
 }
